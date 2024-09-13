6592b8eb44606be8ebd2646070207219b68b8f18
DiscM <fusionfall.salabao@gmail.com>
2024-09-13 13:44:49 -0700

    Implemented Card Draw
    Added in card draw and discard function, but issue with Warrior resource is preventing it from working
    "Invalid call. Nonexistent function 'size' in base 'Resource (warrior_block.gd)'."

--- scenes/battle/battle.gd
+++ scenes/battle/battle.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 @export var char_stats: CharacterStats
 @onready var battle_ui: BattleUI = $BattleUI as BattleUI
+@onready var player_handler: PlayerHandler = $PlayerHandler as PlayerHandler
 
 func _ready() -> void:
 	var new_stats: CharacterStats = char_stats.create_instance()
 	battle_ui.char_stats = new_stats
 	
+	Events.player_turn_ended.connect(player_handler.end_turn)
+	Events.player_hand_discarded.connect(player_handler.start_turn)
+	
 	start_battle(new_stats)
 	
 func start_battle(stats: CharacterStats) -> void:
-	print("The battle has started!")
+	player_handler.start_battle(stats)
